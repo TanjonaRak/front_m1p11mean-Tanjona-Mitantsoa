9c97df37ac7f5e5df8db5ccd67398e58d09cebdb
mitantsooa <99263685+mitantsooa@users.noreply.github.com>
2024-02-25 21:57:30 +0300

    front
    front

--- src/app/Front-Office/login-client/login-client.component.ts
+++ src/app/Front-Office/login-client/login-client.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { CustomersApiService } from 'src/app/service/Customers/customers-api.service';
 
 @Component({
   selector: 'app-login-client',
@@ -7,10 +8,17 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./login-client.component.css']
 })
 export class LoginClientComponent {
+  constructor (private customer_api:CustomersApiService){}
+  
 
-  
-  
   Connect(form:NgForm){
+    try {
+      this.customer_api.LogingProcessing(form.value.email,form.value.pass).subscribe((res:any)=>{
+        console.log(JSON.stringify(res))  
+      });
+    } catch (error) {
+      console.log(error);
+    }
       
   }
 
