a3faa981490122919e2b35c3f4131acf508d87d5
mitantsooa <99263685+mitantsooa@users.noreply.github.com>
2024-02-26 18:12:09 +0300

    comit login

--- src/app/service/Customers/customers-api.service.ts
+++ src/app/service/Customers/customers-api.service.ts
@@ -11,6 +11,14 @@ export class CustomersApiService {
   url_base = "/customer";
   constructor(private httpClient:HttpClient) { }
 
+  LogingProcessing (email: string,pass : string){
+    try {
+      let log = this.httpClient.post(url+this.url_base,+"/"+email+"/"+pass);
+      return log;
+    } catch (error) {
+       throw error;
+    }
+  }
   SaveCustomer (cust :  Customers){
     try {
       let res = this.httpClient.post(url+this.url_base,JSON.parse(JSON.stringify(cust)));
